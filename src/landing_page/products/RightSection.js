58db522d2454eb6bfce5d9d2dfc974361917f127
Ganesh Pasagada <ganeshpasagada.1248@gmail.com>
2025-07-21 00:29:34 +0530

    Updated Hero, ProductPage, RightSection, and structured Universe component

--- src/landing_page/products/RightSection.js
+++ src/landing_page/products/RightSection.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
+function RightSection({ imageUrl, productName, productDescription, learnMore }) {
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-6 p-5 mt-5">
+                    <h2 className="text-muted fs-3">{productName}</h2>
+                    <p className="mt-5">{productDescription}</p>
 
-function RightSection() {
-    return ( 
-        <h1>Right Section</h1>
-     );
+                    <div style={{ marginBottom: '5px' }}>
+                       
+
+                        <a
+                            href={learnMore}
+                            style={{ textDecoration: 'none' }}
+                        >
+                            {learnMore} <i className="fa-solid fa-arrow-right-long"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="col-6">
+                    <img src={imageUrl} alt="Product visual" style={{ maxWidth: '100%', borderRadius: '8px' }} />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default RightSection;
