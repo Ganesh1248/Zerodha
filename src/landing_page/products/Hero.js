0698f31ac309aebeefe4d924a4b21d79d414340e
Ganesh Pasagada <ganeshpasagada.1248@gmail.com>
2025-07-20 13:46:01 +0530

    Updated Hero, LeftSection, and ProductPage in Home Component

--- src/landing_page/products/Hero.js
+++ src/landing_page/products/Hero.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className="container text-center mt-5 mb-5 p-5 text-muted border-bottom"> 
+            <h3>Zerodha Products</h3>
+            <p className="text-muted mt-3 fs-5 mb-5">Sleek, modern, and intuitive trading platforms</p>
+            <p className='mt-3'>Check out our <a href = '' style = {{textDecoration:"none"}}>investment offerings <i class="fa-solid fa-arrow-right-long"></i></a></p>
+        </div>
      );
 }
 
--- src/landing_page/products/LeftSection.js
+++ src/landing_page/products/LeftSection.js
@@ -1,8 +1,43 @@
 import React from 'react';
 
-function LeftSection() {
+function LeftSection({imageUrl, productName,productDescription, tryDemo, learnMore, googlePlay, appStore}) {
     return ( 
-        <h1>Left Section</h1>
+        
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-6 ">
+                    <img src={imageUrl}  />
+                </div>
+                <div className="col-6 p-5 mt-5">
+                    <h2 className='text-muted fs-3'>{productName}</h2>
+                    <p className='mt-5'>{productDescription}</p>
+                   <div style={{ marginBottom: '5px' }}>
+                        <a
+                            href={tryDemo}
+                            style={{ textDecoration: 'none' }}
+                        >
+                            Try Demo <i className="fa-solid fa-arrow-right-long"></i>
+                        </a>
+                        <a
+                            href={learnMore}
+                            style={{ marginLeft: '50px', textDecoration: 'none' }}
+                        >
+                            Learn More <i className="fa-solid fa-arrow-right-long"></i>
+                        </a>
+                   </div>
+
+                    
+                    <div className="mt-3">
+                        <a href={googlePlay}><img src ="media/googlePlayBadge.svg"/></a>
+                        <a href={appStore} style={{ marginLeft: '50px' }}><img src ="media/appStoreBadge.svg"/></a>
+                    </div>
+                    
+
+                    
+
+                </div>
+            </div>
+        </div>
      );
 }
 
