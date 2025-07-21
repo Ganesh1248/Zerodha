a5154da19dfc75781d2c50eaa6f3ce471d4de081
Ganesh Pasagada <ganeshpasagada.1248@gmail.com>
2025-07-22 00:35:53 +0530

    did the pricing component today

--- src/landing_page/pricing/Hero.js
+++ src/landing_page/pricing/Hero.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+       <div>
+            <div className="container border-bottom text-center">
+                {/* Info section */}
+                <div className="row mt-5 border-bottom">
+                    <h1 className="fs-2">Pricing</h1>
+                    <p className="text-muted mt-3">
+                        Free equity investment and flat ₹20 intraday and F&O trades
+                    </p>
+                </div>
+                <div className='row p-5 mt-5 text-center'>
+                    <div className="col-4 p-4">
+                        <img src="media/pricingEquity.svg" />
+                        <h1 className='fs-3'>Free equity delivery</h1>
+                        <p className='text-muted'>All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</p>
+                    </div>
+                    <div className="col-4 p-4">
+                         <img src="media/intradayTrades.svg" />
+                        <h1 className='fs-3'>Intraday and F&O trades</h1>
+                        <p className='text-muted'>Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p>
+                    </div>
+                    <div className="col-4 p-4">
+                         <img src="media/pricingEquity.svg" />
+                        <h1 className='fs-3'>Free direct MF</h1>
+                        <p className='text-muted'>All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
      );
 }
 
